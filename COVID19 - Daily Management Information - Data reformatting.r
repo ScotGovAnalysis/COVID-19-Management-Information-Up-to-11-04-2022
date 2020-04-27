ce381e35c41fbe8d9e4878240b4e27191d251ea1
Victoria Ávila <vavilaregidor@gmail.com>
2020-04-27 14:40:36 +0100

    Add files via upload

--- COVID19 - Daily Management Information - Data reformatting.r
+++ COVID19 - Daily Management Information - Data reformatting.r
@@ -4,7 +4,7 @@
 #           statistics.gov.scot and github.com/DataScienceScotland
 # Author - Victoria Avila (victoria.avila@gov.scot)
 # Date created - 17/04/2020
-# Last updated - 26/04/2020
+# Last updated - 27/04/2020
 # ------------------------------------------------------------------------------
 
 
@@ -20,8 +20,8 @@ library(stringr) #str_remove
 # [1] Reading original files from website --------------------------------------
 # URL shouldn't have changed, but it would good to confirm before running the
 # whole code
-url1 <- "https://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/04/trends-in-number-of-people-in-hospital-with-confirmed-or-suspected-covid-19/documents/trends-in-number-of-people-in-hospital-with-confirmed-or-suspected-covid-19/trends-in-number-of-people-in-hospital-with-confirmed-or-suspected-covid-19/govscot%3Adocument/Trends%2Bin%2Bdaily%2BCOVID-19%2Bdata%2B-%2B26%2BApril%2B2020.xlsx"
-url2 <- "https://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/04/trends-in-number-of-people-in-hospital-with-confirmed-or-suspected-covid-19/documents/covid-19-data-by-nhs-board/covid-19-data-by-nhs-board/govscot%3Adocument/COVID-19%2Bdata%2Bby%2BNHS%2BBoard%2B-%2B26%2BApril%2B2020.xlsx"
+url1 <- "https://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/04/trends-in-number-of-people-in-hospital-with-confirmed-or-suspected-covid-19/documents/trends-in-number-of-people-in-hospital-with-confirmed-or-suspected-covid-19/trends-in-number-of-people-in-hospital-with-confirmed-or-suspected-covid-19/govscot%3Adocument/Trends%2Bin%2Bdaily%2BCOVID-19%2Bdata.xlsx"
+url2 <- "https://www.gov.scot/binaries/content/documents/govscot/publications/statistics/2020/04/trends-in-number-of-people-in-hospital-with-confirmed-or-suspected-covid-19/documents/covid-19-data-by-nhs-board/covid-19-data-by-nhs-board/govscot%3Adocument/COVID-19%2Bdata%2Bby%2BNHS%2BBoard.xlsx"
 
 # -- Scotland (SC) --
 GET(url1, write_disk(tf1 <- tempfile(fileext = ".xlsx")))
