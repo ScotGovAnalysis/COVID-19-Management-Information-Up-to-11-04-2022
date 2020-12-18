7a25a7128e813e629629b4d8a68fdfae3cff5755
Miles Drake <44380377+miles-drake@users.noreply.github.com>
2020-12-18 16:27:01 +0000

    Add QA test to ensure the data set downloaded is today's

--- COVID19 - Daily Management Information - Data reformatting.r
+++ COVID19 - Daily Management Information - Data reformatting.r
@@ -223,6 +223,62 @@ raw_HB_table3  <- read_excel(tf2, "Table 3 - Hospital patients", skip = 2)[, -17
 #unlink(tf1)
 #unlink(tf2)
 
+# [3b] Fetch dates last modified for each dataset -------------------------
+
+date_table_last_modified <- function(df) {
+  df[[1]] %>% max(na.rm = TRUE) %>% ymd() %>% 
+    return()
+}
+
+# Scottish data set
+metadata[[1]]$date_last_modified$tables$raw_SC_table1 <- date_table_last_modified(raw_SC_table1)
+metadata[[1]]$date_last_modified$tables$raw_SC_table2 <- date_table_last_modified(raw_SC_table2)
+metadata[[1]]$date_last_modified$tables$raw_SC_table3 <- date_table_last_modified(raw_SC_table3)
+metadata[[1]]$date_last_modified$tables$raw_SC_table4 <- date_table_last_modified(raw_SC_table4)
+metadata[[1]]$date_last_modified$tables$raw_SC_table5 <- date_table_last_modified(raw_SC_table5)
+metadata[[1]]$date_last_modified$tables$raw_SC_table6 <- date_table_last_modified(raw_SC_table6)
+metadata[[1]]$date_last_modified$tables$raw_SC_table7b <- date_table_last_modified(raw_SC_table7b)
+metadata[[1]]$date_last_modified$tables$raw_SC_table8 <- date_table_last_modified(raw_SC_table8)
+metadata[[1]]$date_last_modified$tables$raw_SC_table9 <- date_table_last_modified(raw_SC_table9)
+
+metadata[[1]]$date_last_modified$data_set <- metadata[[1]]$date_last_modified$tables %>% 
+  unlist() %>% max() %>% as.Date(origin = "1970-01-01") %>% ymd()
+
+# Health board data set
+metadata[[2]]$date_last_modified$tables$raw_HB_table1 <- date_table_last_modified(raw_HB_table1)
+metadata[[2]]$date_last_modified$tables$raw_HB_table2 <- date_table_last_modified(raw_HB_table2)
+metadata[[2]]$date_last_modified$tables$raw_HB_table3 <- date_table_last_modified(raw_HB_table3)
+
+metadata[[2]]$date_last_modified$data_set <- metadata[[2]]$date_last_modified$tables %>% 
+  unlist() %>% max() %>% as.Date(origin = "1970-01-01") %>% ymd()
+
+# [3c] Quality assurance --------------------------------------------------
+# Prompt the user if the data sets downloaded do not appear to be today's
+sapply(1:length(metadata), function(i) {
+  
+  name <- names(metadata)[[i]]
+  date_last_modified <- metadata[[i]]$date_last_modified$data_set
+  date_now <- today$iso
+  age <- date_now - date_last_modified
+  
+  if (age != 0) {
+    
+    error_message <- str_c(
+      "Data set number ", i, " (", name, ") does not seem to be the most recent version.\n",
+      "It was last updated on ", date_last_modified, ".\n",
+      "Today is ", date_now, ".\n",
+      "This data set was last updated ", age, " days ago.\n",
+      "Press [Enter] in the console window to continue."
+    )
+    
+    readline(prompt = error_message) %>% invisible()
+    
+  }
+  
+  return()
+  
+})
+
 # [4] Renaming variables -------------------------------------------------------
 SC_table1 <- raw_SC_table1 %>%
   rename("Calls - NHS24 111" = "NHS24 111 Calls",
