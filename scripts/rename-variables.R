ef2081fc88d576a9c6b1cb52fb2306f07ec77049
Miles Drake <44380377+miles-drake@users.noreply.github.com>
2021-03-22 17:48:07 +0000

    Add script to rename columns / variables

---
+++ scripts/rename-variables.R
@@ -0,0 +1,47 @@
+# Read data ---------------------------------------------------------------
+
+for(x in names(data_sets)){
+  
+  if(data_sets[[x]]$flags$import){
+    
+    table_name <- data_sets[[x]]$metadata$table_name
+    
+    print(
+      paste("Renaming variables in table", x, "--", table_name, "..."),
+      quote = FALSE
+    )
+
+    data_sets[[x]]$data$new <- data_sets[[x]]$data$raw
+    
+    names(data_sets[[x]]$data$new) <- data_sets[[x]]$table_structure$col_name_new[data_sets[[x]]$table_structure$col_type != "skip"]
+    
+  }
+  
+}
+
+rm(x, table_name)
+
+# QA: New variable names --------------------------------------------------
+
+for(x in names(data_sets)){
+  
+  if(data_sets[[x]]$flags$import){
+    
+    table_name <- data_sets[[x]]$metadata$table_name
+    variable_names_actual <- names(data_sets[[x]]$data$new)
+    variable_names_expected <- data_sets[[x]]$table_structure$col_name_new[data_sets[[x]]$table_structure$col_type != "skip"]
+    
+    print(
+      paste("Confirming new variable names for table", x, "--", table_name, "..."),
+      quote = FALSE
+    )
+    
+    if(sum(variable_names_actual != variable_names_expected) > 0){
+      warning("After renaming, the data set ", x, " does not have the expected variable names.")
+    }
+    
+  }
+  
+}
+
+rm(x, table_name, variable_names_actual, variable_names_expected)
